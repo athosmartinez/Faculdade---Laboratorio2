32a0e0dd079d0fa59e14f4e5a1813c87b1bcff40
Athos Martinez <athosandrade04@gmail.com>
2022-08-04 10:00:29 -0300

    .

---
+++ Revisao/5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex5 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = 1;
+            Console.WriteLine("Entre com que número deseja calcular o fatorial: ");
+            int num = int.Parse(Console.ReadLine());
+            if(num == 0)
+            {
+                Console.WriteLine("Fatorial de 0 de 1");
+            }
+            else
+            {
+                for(int i = 1; i <= num; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine("Factorial de {0} é {1}", num, factorial);
+            }
+        }
+    }
+}
