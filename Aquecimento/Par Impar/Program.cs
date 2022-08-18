9b676ff90aafdee5be845b625034d60222f58da2
Athos Martinez <athosandrade04@gmail.com>
2022-08-17 23:15:13 -0300

    .

---
+++ Aquecimento/Par Impar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            numero =  int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+            
+        }
+    }
+}
