9ca1aaf88c8fe8c59ddc1bb27ae5705d1daf5282
Athos Martinez <athosandrade04@gmail.com>
2022-08-24 22:41:25 -0300

    .

---
+++ LISTA 1/1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string polindromo,reverse;
+            do
+            {   
+                polindromo = Console.ReadLine();
+                if(polindromo.ToUpper().Equals("FIM")) continue;
+                char[] ch = polindromo.ToCharArray();
+                Array.Reverse(ch);
+                reverse = new string(ch);
+                bool b = polindromo.Equals(reverse, StringComparison.CurrentCulture);
+                if (b == true)
+                {
+                    Console.WriteLine("SIM");
+                }
+                else
+                {
+                    Console.WriteLine("NAO");
+                }
+            } while (!polindromo.ToUpper().Equals("FIM"));
+        }
+    }
+}
