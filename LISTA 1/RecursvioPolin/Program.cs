f2e73719ddd87f68ddbd993d2cc68918a0996278
Athos Martinez <athosandrade04@gmail.com>
2022-08-29 12:07:08 -0300

    .

--- LISTA 1/RecursvioPolin/Program.cs
+++ LISTA 1/RecursvioPolin/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha = "";
+            do
+            {
+                linha = Console.ReadLine();
+            } while (linha != "FIM");
+            System.Console.WriteLine(épolindromo(linha, 0, linha.Length - 1));
+        }
+
+        public static string épolindromo(string frase, int esquerda, int direita)
+        {
+            Boolean resposta = true;
+            while (esquerda < direita)
+            {
+                if (frase[esquerda] == frase[direita])
+                {
+                    épolindromo(frase, esquerda + 1, direita - 1);
+                    resposta = true;
+                }
+                else
+                {
+                    resposta = false;
+                    esquerda = direita;
+                }
+            }
+            return resposta ? "SIM" : "NAO";
+        }
+    }
+}
---
+++ TSTE/1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(int[] args)
+        {
+            int X,
+                Y;
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+        
+        }
+        public static int mdc(int x, int y)
+        {
+            if (x % y == 0)
+            {
+                return y;
+            }
+            else
+            {
+                return mdc(y, x % y);
+            }
+        }
+    }
+}
