4652822078b08e371e17b7d239e93d470abdaeab
Athos Martinez <athosandrade04@gmail.com>
2022-09-01 00:52:57 -0300

    .

--- LISTA 1/RecursivoIsemC#/Program.cs
+++ LISTA 1/RecursivoIsemC#/Program.cs
@@ -43,7 +43,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 && palavra[i] != 'i'
                 && palavra[i] != 'o'
                 && palavra[i] != 'u'
-
             )
             {
                 return "NAO";
