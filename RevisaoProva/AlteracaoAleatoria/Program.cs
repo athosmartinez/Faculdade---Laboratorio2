7639095b6b5e562daef543aa4e95f829926d87ea
Athos Martinez <athosandrade04@gmail.com>
2022-10-05 16:59:54 -0300

    .

---
+++ RevisaoProva/AlteracaoAleatoria/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase, print = "";
+            do
+            {
+                Random generator = new Random(4);
+                char random0 = (char)('a' + (Math.Abs(generator.Next()) % 26));
+                char random1 = (char)('a' + (Math.Abs(generator.Next()) % 26));
+                phrase = Console.ReadLine();
+                if (phrase == "FIM") continue;
+                for (int i = 0; i < phrase.Length; i++)
+                {
+                    if (phrase[i] == ' ')
+                    {
+                        print += " ";
+                    }
+                    else if(phrase[i] == random0)
+                    {
+                        print += random1;
+                    }
+                    else if(phrase[i] == random1)
+                    {
+                        print += random0;
+                    }
+                    else
+                    {
+                        print += phrase[i];
+                    }
+                }
+                Console.WriteLine(print);
+                print = "";
+            } while (phrase != "FIM");
+
+
+        }
+    }
+}
---
+++ RevisaoProva/ISemCRecursivo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = " ";
+            while (phrase != "FIM")
+            {
+                phrase = Console.ReadLine();
+                if (phrase != "FIM")
+                {
+                    Console.WriteLine(vogal(phrase)
+                    + " " + consoante(phrase)
+                    + " " + numero(phrase)
+                    + " " + real(phrase));
+                }
+            }
+        }
+
+        public static string vogal(string phrase)
+        {
+            //recursive function to check if the phrase has a vowel
+            if (phrase.Length == 0)
+            {
+                return "NAO";
+            }
+            else if (phrase[0] == 'a' || phrase[0] == 'e' || phrase[0] == 'i' || phrase[0] == 'o' || phrase[0] == 'u')
+            {
+                return "SIM";
+            }
+            else
+            {
+                return vogal(phrase.Substring(1));
+            }
+        }
+
+        public static string consoante(string phrase)
+        {
+            //recursive function to check if the phrase has a consonant
+            if (phrase.Length == 0)
+            {
+                return "NAO";
+            }
+            else if (phrase[0] != 'a' && phrase[0] != 'e' && phrase[0] != 'i' && phrase[0] != 'o' && phrase[0] != 'u')
+            {
+                return "SIM";
+            }
+            else
+            {
+                return consoante(phrase.Substring(1));
+            }
+
+        }
+
+        static string numero(string phrase)
+        {
+            //recursive function to check if the phrase has a number
+            int numero;
+            return int.TryParse(phrase, out numero) ? "SIM" : "NAO";
+        }
+
+
+        static string real(string phrase)
+        {
+            //recursive function to check if the phrase has a real number
+            double real;
+            return double.TryParse(phrase, out real) ? "SIM" : "NAO";
+
+
+        }
+    }
+
+}
