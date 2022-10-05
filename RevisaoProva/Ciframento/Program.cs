ab802be12178f6c896c3b6d3e3fa92e8d752873b
Athos Martinez <athosandrade04@gmail.com>
2022-10-05 12:19:15 -0300

    .

---
+++ RevisaoProva/Ciframento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            string encrypted = "";
+            //encrypt the string changing char for 3 char after
+            do
+            {
+                str = Console.ReadLine();
+                if (str == "FIM") continue;
+                foreach (char c in str)
+                {
+                    encrypted += (char)(c + 3);
+                }
+                Console.WriteLine(encrypted);
+                encrypted = "";
+            } while (str != "FIM");
+
+
+
+        }
+    }
+}
