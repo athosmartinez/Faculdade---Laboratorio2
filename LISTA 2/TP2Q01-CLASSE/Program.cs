f1a6bed771543482099db503594700b4c6e9ac77
Athos Martinez <athosandrade04@gmail.com>
2022-10-28 07:06:49 -0300

    .

--- LISTA 2/TP2Q01-CLASSE/Program.cs
+++ LISTA 2/TP2Q01-CLASSE/Program.cs
@@ -19,8 +19,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         public void Ler(string entrada)
         {
-        
             // Divisoes em Array[] - Parte esquerda
+
             string divisaoEsquerda = entrada.Split("[")[0];
             string divisaoDireita = entrada.Split("[")[1];
             string[] dividido = divisaoEsquerda.Split(",");
@@ -28,14 +28,18 @@ namespace MyApp // Note: actual namespace depends on the project name.
             this.Nome = dividido[1];
             this.Foto = dividido[2];
             this.Id = Int32.Parse(dividido[5]);
-            this.Nascimento = DateTime.Parse(dividido[3]);
+            // Divisoes da data em partes 
+            string[] Dados = dividido[4].Split("/");
+            this.Nascimento = (new DateTime(Int32.Parse(Dados[2]), Int32.Parse(Dados[1]), Int32.Parse(Dados[0])));
+
+            // this.Nascimento = DateTime.Parse(dividido[3]);
 
             // Divisoes em Array[] - Parte direita
+
             string[] dividido2 = divisaoDireita.Substring(0, divisaoDireita.IndexOf("]")).Split(",");
             for (int i = 0; i < dividido2.Length; i++)
             {
                 this.Times[i] = Int32.Parse(dividido2[i]);
-
             }
         }
 
@@ -44,13 +48,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
             string saida = "";
             for (int i = 0; i < 30; i++)
             {
-                if (Times[i] != 0){
+                if (Times[i] != 0)
+                {
                     saida += this.Times[i];
                 }
             }
             Console.WriteLine($"{this.Id} {this.Nome} {this.Nascimento} {this.Foto} ({saida})");
         }
-
     }
     internal class Program
     {
@@ -62,7 +66,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 palavra = Console.ReadLine();
                 if (palavra == "FIM")
                     continue;
-
                 Jogadores jogador = new Jogadores();
                 jogador.Ler(palavra);
                 jogador.Imprimir();
