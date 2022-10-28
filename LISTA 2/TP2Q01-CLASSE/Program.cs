5b0cc096704a9336099c3aeb0bf59e98ee8a809b
Athos Martinez <athosandrade04@gmail.com>
2022-10-28 11:09:41 -0300

    .

--- LISTA 2/TP2Q01-CLASSE/Program.cs
+++ LISTA 2/TP2Q01-CLASSE/Program.cs
@@ -17,6 +17,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
             this.Times = new int[30];
         }
 
+        public DateTime GetNascimento()
+        {
+            return Nascimento;
+        }
+        public void SetNascimento(DateTime nascimento)
+        {
+            this.Nascimento = Nascimento;
+        }
+
         public void Ler(string entrada)
         {
             // Divisoes em Array[] - Parte esquerda
@@ -28,11 +37,12 @@ namespace MyApp // Note: actual namespace depends on the project name.
             this.Nome = dividido[1];
             this.Foto = dividido[2];
             this.Id = Int32.Parse(dividido[5]);
-            // Divisoes da data em partes 
-            string[] Dados = dividido[4].Split("/");
-            this.Nascimento = (new DateTime(Int32.Parse(Dados[2]), Int32.Parse(Dados[1]), Int32.Parse(Dados[0])));
 
-            // this.Nascimento = DateTime.Parse(dividido[3]);
+            // Divisoes da data em partes 
+
+
+            string[] Dados = dividido[3].Split("/");
+            SetNascimento(new DateTime(Int32.Parse(Dados[2]), Int32.Parse(Dados[1]), Int32.Parse(Dados[0])));
 
             // Divisoes em Array[] - Parte direita
 
@@ -42,18 +52,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 this.Times[i] = Int32.Parse(dividido2[i]);
             }
         }
-
         public void Imprimir()
         {
+            string data = GetNascimento().ToString("dd/MM/yyyy");
+            string newData = data.Substring(0, 2).TrimStart('0') + data.Substring(2, data.Length - 2);
             string saida = "";
+            string timeP ="(";
             for (int i = 0; i < 30; i++)
             {
                 if (Times[i] != 0)
                 {
-                    saida += this.Times[i];
+                    saida += this.Times[i] + (i == Times.Length - 1 ? "" : ",");
                 }
             }
-            Console.WriteLine($"{this.Id} {this.Nome} {this.Nascimento} {this.Foto} ({saida})");
+            Console.WriteLine($"{this.Id} {this.Nome} {newData} {this.Foto} ({saida})");
         }
     }
     internal class Program
