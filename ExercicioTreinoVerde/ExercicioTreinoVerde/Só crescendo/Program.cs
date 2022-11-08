77b5653b1b4c02309c80f36182eb16edf45aac5d
Athos Martinez <athosandrade04@gmail.com>
2022-11-08 14:29:54 -0300

    .

---
+++ ExercicioTreinoVerde/ExercicioTreinoVerde/Só crescendo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayInt[] inteiros = new ArrayInt[50];
+        }
+        class ArrayInt
+        {
+            int numeroTeste;
+            int numeroTamanho;
+        }
+
+        public void Ler(string nTeste, string nTamanho)
+        {
+            nTeste = Console.ReadLine();
+            nTamanho = Console.ReadLine();
+            int numeroTeste = Convert.ToInt32(nTeste);
+            int numeroTamanho = Convert.ToInt32(nTamanho);
+            
+            for (int i = 0; i < numeroTeste; i++)
+            {
+                for (int j = 0; j < numeroTamanho; j++)
+                {
+                    inteiros[i].numeroTeste = numeroTeste;
+                    inteiros[i].numeroTamanho = numeroTamanho;
+                }
+            }
+        }
+
+    }
+}
